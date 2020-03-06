af0f88986f5ef7eb2154e2b16f38582297324300
zcy <zcy@DESKTOP-FI09EKC>
2020-03-06 10:03:06 +0800

    Create

---
+++ src/CreateRegedit/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+namespace CreateRegedit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HKEY_CLASSES_ROOT\Directory\Background\shell
+            RegistryKey key = Registry.ClassesRoot;
+            RegistryKey directory = key.OpenSubKey("Directory");
+            RegistryKey Background = directory.OpenSubKey("Background");
+            RegistryKey shell = Background.OpenSubKey("shell", true);
+            
+            try
+            {
+                if (!shell.GetSubKeyNames().Contains("Menu_Date"))
+                {
+
+                    Console.WriteLine("开始写入注册表");
+
+                    RegistryKey Menu_Date = shell.CreateSubKey("Menu_Date");
+                    Menu_Date.SetValue("", "新建日期文件夹");
+                    var command = Menu_Date.CreateSubKey("command");
+                    command.SetValue("", @"C:\ContextMenu\Menu_Date.exe");
+                    Console.WriteLine("新建右键菜单“新建日期文件夹” Success");
+
+
+                }
+                if (!shell.GetSubKeyNames().Contains("Menu_Time"))
+                {
+                    Console.WriteLine("开始写入注册表");
+
+                    RegistryKey Menu_Time = shell.CreateSubKey("Menu_Time");
+                    Menu_Time.SetValue("", "新建时间文件夹");
+                    var command = Menu_Time.CreateSubKey("command");
+                    command.SetValue("", @"C:\ContextMenu\Menu_Time.exe");
+
+                    Console.WriteLine("新建右键菜单“新建时间文件夹” Success");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("发生错误：" + ex.ToString());
+
+            }
+            Console.WriteLine("可以关闭当前窗口了");
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+}
---
+++ src/Menu_Date/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Menu_Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cPath = Directory.GetCurrentDirectory();
+            string dir = DateTime.Now.ToString("yyyy-MM-dd");
+            var NameList = Directory.GetDirectories(cPath).Select(p => Path.GetFileName(p)).ToList();
+            var i = 0;
+            while (NameList.Exists(p => p == (dir + (i == 0 ? "" : ("-" + i)))))
+            {
+                i++;
+            }
+            if (i == 0)
+            {
+                Directory.CreateDirectory(Path.Combine(cPath, dir));
+            }
+            else
+            {
+                Directory.CreateDirectory(Path.Combine(cPath, $"{dir}-{i}"));
+            }
+        }
+    }
+}
